9bcaec19e12395ed2c36ca5640ee408a306be2f4
Khadir Abdul <khadir786.ka@googlemail.com>
2024-12-02 21:43:07 +0000

    refactored class components to functional components

--- src/main/js/app.jsx
+++ src/main/js/app.jsx
@@ -1,34 +1,34 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client';
+import React, {useState, useEffect} from 'react'
+import {createRoot} from "react-dom/client";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import BasicTable from "./components/BasicTable.jsx";
 import './app.css';
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {data: []}
-    }
 
-    componentDidMount() {
-        this.fetchData()
-    }
+function App() {
+    const [data, setData] = useState([]);
 
-    render() {
-        // You can use this.state.data to access your data from your state
-        return (<BasicTable/>)
-    }
+    useEffect(fetchData, [])
 
-    fetchData = () =>  {
-        fetch('/people')
-            .then(response => response.json())
-            .then(data => {
-                this.setState({
+    const fetchData = () => {
+        try {
+            fetch('/people')
+                .then(response => response.json())
+                .then(data => {
                     // set your data here once you're happy for it to pass to the table
+                    setData(data);
                 });
-            });
+        } catch (error) {
+            console.log(error);
+        }
+
     }
+
+    return (
+        <BasicTable/>
+    )
 }
 
-
 createRoot(document.getElementById('app')).render(<App/>);
+export default App;
+
--- src/main/js/components/BasicTable.jsx
+++ src/main/js/components/BasicTable.jsx
@@ -1,23 +1,21 @@
 import React from "react";
 import {Container, Table} from "reactstrap";
 
-class BasicTable extends React.Component {
+function BasicTable() {
     // feed your data here, and build your table around that data
-    render() {
-        return (<Container fluid>
-                <Table className="mt-4">
-                    <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Description</th>
-                        <th>Actions</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    </tbody>
-                </Table>
-            </Container>)
-    }
+    return (<Container fluid>
+        <Table className="mt-4">
+            <thead>
+            <tr>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </Table>
+    </Container>)
 }
 
 export default BasicTable
