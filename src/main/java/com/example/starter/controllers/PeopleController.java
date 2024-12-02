3066b6b857551c917a37126b793ced612597afed
Khadir Abdul <khadir786.ka@googlemail.com>
2024-12-02 23:39:13 +0000

    completed api request for star wars /people

--- src/main/java/com/example/starter/controllers/PeopleController.java
+++ src/main/java/com/example/starter/controllers/PeopleController.java
@@ -1,14 +1,25 @@
 package com.example.starter.controllers;
 
+import com.example.starter.service.PeopleService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // This annotation tells Spring that this is a controller.
 public class PeopleController {
+    private final PeopleService peopleService;
+
+    public PeopleController(PeopleService userService) {
+        this.peopleService = userService;
+    }
 
     @GetMapping("/people") // This annotation tells Spring that this method should be called when a GET request is made to "/people".
     public String getPeople() {
+        try {
+            return peopleService.getPeople();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         // This will need to connect to the People service.
-        throw new UnsupportedOperationException("Not yet implemented");
     }
 }
--- src/main/java/com/example/starter/service/PeopleService.java
+++ src/main/java/com/example/starter/service/PeopleService.java
@@ -1,9 +1,26 @@
 package com.example.starter.service;
 
-// You need an annotation here to tell Spring that this is a service.
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.*;
+
+// You need an annotation here to tell Spring that this is a service.
+@Service
 public class PeopleService {
     final String apiUrl = "https://swapi.dev/api/people/";
+    private Map<String, Integer> peopleIDs = new HashMap<String, Integer>();
+
+    // if you know the IDs for the person, they can be added here
+    PeopleService() {
+        peopleIDs.put("Luke Skywalker", 1);
+        peopleIDs.put("C-3PO", 2);
+        peopleIDs.put("R2-D2", 3);
+        peopleIDs.put("Darth Vader", 4);
+    }
 
     /**
      * This method should return a list of people from the Star Wars API.
@@ -17,7 +34,27 @@ public class PeopleService {
      * </ol>
      * @return A list of people from the Star Wars API.
      */
-    public String getPeople() {
-        throw new UnsupportedOperationException("Not yet implemented");
+    public String getPeople() throws JsonProcessingException {
+        RestTemplate restTemplate = new RestTemplate();
+        List<Map<String, Object>> combinedResponse = new ArrayList<>();
+
+        // iterate over each character and fetch their data
+        for (Map.Entry<String, Integer> entry : peopleIDs.entrySet()) {
+            String characterName = entry.getKey();
+            int characterId = entry.getValue();
+
+            try {
+                // fetch data for each character and put into a map... map -> json object (each map is its own json object)
+                ResponseEntity<Map> response = restTemplate.getForEntity(apiUrl + characterId, Map.class);
+                combinedResponse.add(response.getBody());
+            } catch (Exception e) {
+                System.err.println("Failed to fetch data for " + characterName + ": " + e.getMessage());
+            }
+        }
+
+        // convert the combined response to a json string
+        return new ObjectMapper().writeValueAsString(combinedResponse);
     }
+
 }
+// could just make it into an actual json object to return... would have to change too many things
