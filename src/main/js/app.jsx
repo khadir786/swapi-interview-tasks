7117fcd1ea7d45d1d3eba9705a1b25c51440083f
Khadir Abdul <khadir786.ka@googlemail.com>
2024-12-03 20:35:45 +0000

    created search area and results area that interacts with api

--- src/main/js/app.jsx
+++ src/main/js/app.jsx
@@ -3,12 +3,19 @@ import {createRoot} from "react-dom/client";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import BasicTable from "./components/BasicTable.jsx";
 import './app.css';
-import {Container} from "reactstrap";
+import {Button, Container} from "reactstrap";
+import LoadingIcon from "./components/LoadingIcon.jsx";
 
 
 function App() {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [isSearchLoading, setIsSearchLoading] = useState(false);
+    const [currentCharacterName, setCurrentCharacterName] = useState("");
+    const [characterData, setCharacterData] = useState([]);
+    const [isError, setIsError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [searchResults, setSearchResults] = useState("");
 
     useEffect(() => {
         fetchData()
@@ -23,35 +30,81 @@ function App() {
                     console.log(data);
                     setIsLoading(false);
                     for (const character of data) {
-                        console.log(character.name)
+                        console.log(character.name);
                     }
                 });
 
         } catch (error) {
             console.log(error);
         }
+    }
 
+    const searchCharacter = () => {
+        if (currentCharacterName.trim() === "") {
+            console.log("nothing in here man")
+            setIsSearchLoading(prevState => !prevState);
+            setIsError(true);
+            return;
+        }
+        try {
+            fetch(`/person/?name=${currentCharacterName}`)
+                .then(response => response.json())
+                .then(data => {
+                    setCharacterData(data);
+                    setSearchResults(data.results)
+                    console.log(data);
+                    setIsSearchLoading(false);
+                })
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const handleSearch = () => {
+        setIsSearchLoading(true);
+        searchCharacter();
     }
 
     return (
         <div className="App">
-            {
-                isLoading ?
-                    <div className="loading" style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
-                        <div className="spinner-border" role="status">
-                            {/*<span className="sr-only">Loading</span>*/}
-                        </div>
-                    </div> :
-
-                    <Container fluid>
-                        <BasicTable data={data}/>
-
-                    </Container>
-            }
+            {isLoading ? <LoadingIcon center={true}/> : <Container fluid>
+                <BasicTable data={data}/>
+                <div className="SearchArea">
+                    <div className="SearchBox">
+                        <form>
+                            <input
+                                type="text"
+                                placeholder="Character Name"
+                                name="characterName"
+                                onChange={(e) => setCurrentCharacterName(e.target.value)}
+                            />
+                        </form>
+                        <button
+                            type="button"
+                            className="btn btn-secondary"
+                            onClick={handleSearch}
+                        >
+                            Search
+                        </button>
+                    </div>
+                    <div className="ResultsArea">
+                        {isSearchLoading ? <LoadingIcon/> : characterData.count > 0 ? (
+                            <div className="Results">
+                                <p>Found {characterData.count} results</p>
+                                <ul>
+                                    {searchResults.map((character, index) => (
+                                        <li key={index}>{character.name}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                        ) : (isError && <p>No results</p>)
+                        }
+                    </div>
+                </div>
+            </Container>}
         </div>
-
-
-    )
+    );
 }
 
 createRoot(document.getElementById('app')).render(<App/>);
